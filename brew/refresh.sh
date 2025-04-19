58ef8d4f26f29b835f9d6798060fa16f7d225f01
Tomasz Mycielski <76878490+mycielski@users.noreply.github.com>
2025-04-19 20:18:12 +0200

    update

--- brew/refresh.sh
+++ brew/refresh.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 script_dir="$(cd "$(dirname "$0")" && pwd)"
-brew list >"$script_dir/list.txt"
-brew list --cask >"$script_dir/list--cask.txt"
-brew leaves >"$script_dir/leaves.txt"
-brew bundle dump --force --file="$script_dir/Brewfile"
+brew list >"$script_dir/list.txt" &
+brew list --cask >"$script_dir/list--cask.txt" &
+brew leaves >"$script_dir/leaves.txt" &
+brew bundle dump --force --file="$script_dir/Brewfile" &
+
+wait
 
 (
 	cd "$script_dir" &&
