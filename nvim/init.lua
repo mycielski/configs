611cddb3868713e840930e7c618c0b87c8ec6d0f
Tomasz Mycielski <76878490+mycielski@users.noreply.github.com>
2025-04-19 19:05:04 +0200

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,11 @@
+--------------------------------
+-- sync clipboard with system --
+--------------------------------
 vim.opt.clipboard = "unnamedplus"
 
+------------------
+-- line numbers --
+------------------
 vim.opt.number = true
 vim.opt.relativenumber = true
+
