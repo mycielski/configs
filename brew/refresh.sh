70ae3c59bc2e807dae9a35ad18eae4ccf48ca1c7
Tomasz Mycielski <76878490+mycielski@users.noreply.github.com>
2025-04-19 19:58:32 +0200

    refresh

--- brew/refresh.sh
+++ brew/refresh.sh
@@ -5,3 +5,5 @@ brew list >"$script_dir/list.txt"
 brew list --cask >"$script_dir/list--cask.txt"
 brew leaves >"$script_dir/leaves.txt"
 brew bundle dump --force --file="$script_dir/Brewfile"
+
+(cd "$script_dir" && git add . && git commit -S -m "refresh" && git push)
