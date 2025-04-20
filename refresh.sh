644389a0974ec282a45cca092083b471bde3c0e8
Tomasz Mycielski <76878490+mycielski@users.noreply.github.com>
2025-04-20 21:32:46 +0200

    refresh

--- refresh.sh
+++ refresh.sh
@@ -5,6 +5,7 @@ script_dir="$(cd "$(dirname "$0")" && pwd)"
 ########
 # brew #
 ########
+echo ">> Dumping brew state"
 brew list >"$script_dir/brew/list.txt" &
 brew list --cask >"$script_dir/brew/list--cask.txt" &
 brew leaves >"$script_dir/brew/leaves.txt" &
@@ -14,13 +15,20 @@ brew bundle dump --force --file="$script_dir/brew/Brewfile" &
 # await for all jobs to finish #
 ################################
 wait
+echo ">> Brew state dumped"
 
 ################
 # push to repo #
 ################
 (
 	cd "$script_dir" &&
-		git add . &&
-		git commit -S -m "refresh" &&
-		git push
+		if [[ -n $(git status --short) ]]; then
+			echo ">> Commiting configs to git" &&
+				git add . &&
+				git commit -S -m "refresh" &&
+				git push &&
+				echo ">> Configs pushed to remote repository"
+		else
+			echo ">> Configs already up-to-date in remote"
+		fi
 )
