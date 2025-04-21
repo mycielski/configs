ce50bebb31f32623b81aa2168865fee6733ec535
Tomasz Mycielski <76878490+mycielski@users.noreply.github.com>
2025-04-22 01:25:46 +0200

    chore: refresh crontab config

--- crontab/refresh_crontab.sh
+++ crontab/refresh_crontab.sh
@@ -2,6 +2,8 @@
 
 script_dir="$(cd "$(dirname "$0")" && pwd)"
 
+whoami  # TODO remove
+
 ###########
 # crontab #
 ###########
@@ -10,7 +12,7 @@ if [ "$(id -u)" -eq 0 ]; then
 	# root #
 	echo "$0 >> Dumping root's crontab -l"
 	crontab -l >"$script_dir/root"
-	sudo --user "$USER" "$0"
+	sudo --user "$SUDO_USER" "$0"
 	exit
 fi
 ########
