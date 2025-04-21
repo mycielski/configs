0fa93c035340b2ed1650f8f47ed3c25497224e85
Tomasz Mycielski <76878490+mycielski@users.noreply.github.com>
2025-04-22 00:44:53 +0200

    chore: refresh crontab config

---
+++ crontab/refresh_crontab.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+
+script_dir="$(cd "$(dirname "$0")" && pwd)"
+
+###########
+# crontab #
+###########
+if [ "$(id -u)" -eq 0 ]; then
+	########
+	# root #
+	echo "$0 >> Dumping root's crontab -l"
+else
+	########
+	# user #
+	echo "$0 >> Dumping user's crontab -l"
+	crontab -l >"$script_dir/user"
+fi
+
+################
+# push to repo #
+################
+(
+	cd "$script_dir" &&
+		if [[ -n $(git status --short) ]]; then
+			echo "$0 >> Commiting configs to git" &&
+				git add . &&
+				git commit -S -m "chore: refresh crontab config" &&
+				git push &&
+				echo "$0 >> Configs pushed to remote repository"
+		else
+			echo "$0 >> Configs already up-to-date in remote"
+		fi
+)
