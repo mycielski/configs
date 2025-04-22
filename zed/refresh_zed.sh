d59f897a83868693b826f33ed7c234b562abd2ab
Tomasz Mycielski <76878490+mycielski@users.noreply.github.com>
2025-04-22 02:59:23 +0200

    chore: refresh zed config

---
+++ zed/refresh_zed.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+script_dir="$(cd "$(dirname "$0")" && pwd)"
+
+########
+# tmux #
+########
+echo "$0 >> Dumping keymap.json and settings.json"
+zed_config_dir="$HOME/.config/zed"
+cp "$zed_config_dir/keymap.json" "$script_dir" &
+cp "$zed_config_dir/settings.json" "$script_dir" &
+
+wait
+
+################
+# push to repo #
+################
+(
+	cd "$script_dir" &&
+		if [[ -n $(git status --short .) ]]; then
+			echo "$0 >> Commiting configs to git" &&
+				git add . &&
+				git commit -S -m "chore: refresh zed config" &&
+				echo "$0 >> Changes commited"
+		else
+			echo "$0 >> Configs already up-to-date in remote"
+		fi
+)
