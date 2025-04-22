d7642b640a39b79e9a8c3e005d33852148fc45e2
Tomasz Mycielski <76878490+mycielski@users.noreply.github.com>
2025-04-22 02:59:38 +0200

    chore: refresh crontab config

--- crontab/refresh_crontab.sh
+++ crontab/refresh_crontab.sh
@@ -10,9 +10,9 @@ if [ "$(id -u)" -eq 0 ]; then
 	# root #
 	echo "$0 >> Dumping root's crontab -l"
 	crontab -l >"$script_dir/root" &
- 	sudo --user "$SUDO_USER" "$0" &
+	sudo --user "$SUDO_USER" "$0" &
 	wait
- 	exit
+	exit
 fi
 
 ########
@@ -29,9 +29,8 @@ crontab -l >"$script_dir/user"
 			echo "$0 >> Commiting configs to git" &&
 				git add . &&
 				git commit -S -m "chore: refresh crontab config" &&
-				git push &&
-				echo "$0 >> Configs pushed to remote repository"
+				echo "$0 >> Changes commited"
 		else
 			echo "$0 >> Configs already up-to-date in remote"
 		fi
-) || true
+)
