44de662254d55cc888a244fd3f1b5b3cd7130ad3
Tomasz Mycielski <76878490+mycielski@users.noreply.github.com>
2025-04-22 02:59:42 +0200

    chore: refresh brew configs

--- brew/refresh_brew.sh
+++ brew/refresh_brew.sh
@@ -21,12 +21,11 @@ wait
 ################
 (
 	cd "$script_dir" &&
-		if [[ -n $(git status --short) ]]; then
+		if [[ -n $(git status --short .) ]]; then
 			echo "$0 >> Commiting configs to git" &&
 				git add . &&
 				git commit -S -m "chore: refresh brew configs" &&
-				git push &&
-				echo "$0 >> Configs pushed to remote repository"
+				echo "$0 >> Changes commited"
 		else
 			echo "$0 >> Configs already up-to-date in remote"
 		fi
