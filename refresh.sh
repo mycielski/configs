60a27fa572650f8b4c052a42d46883d579318da9
Tomasz Mycielski <76878490+mycielski@users.noreply.github.com>
2025-04-22 12:12:39 +0200

    chore: general updates

--- refresh.sh
+++ refresh.sh
@@ -2,7 +2,7 @@
 
 script_dir="$(cd "$(dirname "$0")" && pwd)"
 
-find "$script_dir" -type f -not -path "*.git*" -path "$script_dir/*/refresh_*.sh" #| xargs -n1 -P0 sh
+find "$script_dir" -type f -path "$script_dir/.git" -prune -path "$script_dir/*/refresh_*.sh" | xargs -n1 -P0 sh
 
 git add "$script_dir/**/*.md"
 git commit -S -m "docs: general updates"
