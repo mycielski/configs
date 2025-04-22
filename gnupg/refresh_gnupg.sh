595ca12a3147d0706aeb9d31f5b93b40832aacde
Tomasz Mycielski <76878490+mycielski@users.noreply.github.com>
2025-04-22 02:59:44 +0200

    chore: refresh gpg agent config

--- gnupg/refresh_gnupg.sh
+++ gnupg/refresh_gnupg.sh
@@ -2,11 +2,11 @@
 
 script_dir="$(cd "$(dirname "$0")" && pwd)"
 
-########
-# btop #
-########
+#########
+# gnupg #
+#########
 echo "$0 >> Dumping gpg-agent.conf state"
-cp "$HOME/.gnupg/gpg-agent.conf" "$script_dir/gpg-agent.conf"
+cp "$HOME/.gnupg/gpg-agent.conf" "$script_dir"
 
 ################
 # push to repo #
@@ -17,8 +17,7 @@ cp "$HOME/.gnupg/gpg-agent.conf" "$script_dir/gpg-agent.conf"
 			echo "$0 >> Commiting configs to git" &&
 				git add . &&
 				git commit -S -m "chore: refresh gpg agent config" &&
-				git push &&
-				echo "$0 >> Configs pushed to remote repository"
+				echo "$0 >> Changes commited"
 		else
 			echo "$0 >> Configs already up-to-date in remote"
 		fi
