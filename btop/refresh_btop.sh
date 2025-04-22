2476f85933e535399dd258e2868e9dd393442ff5
Tomasz Mycielski <76878490+mycielski@users.noreply.github.com>
2025-04-22 02:59:43 +0200

    chore: refresh btop config

--- btop/refresh_btop.sh
+++ btop/refresh_btop.sh
@@ -6,19 +6,18 @@ script_dir="$(cd "$(dirname "$0")" && pwd)"
 # btop #
 ########
 echo "$0 >> Dumping btop.conf state"
-cp "$HOME/.config/btop/btop.conf" "$script_dir/btop.conf"
+cp "$HOME/.config/btop/btop.conf" "$script_dir"
 
 ################
 # push to repo #
 ################
 (
 	cd "$script_dir" &&
-		if [[ -n $(git status --short) ]]; then
+		if [[ -n $(git status --short .) ]]; then
 			echo "$0 >> Commiting configs to git" &&
 				git add . &&
 				git commit -S -m "chore: refresh btop config" &&
-				git push &&
-				echo "$0 >> Configs pushed to remote repository"
+				echo "$0 >> Changes commited"
 		else
 			echo "$0 >> Configs already up-to-date in remote"
 		fi
