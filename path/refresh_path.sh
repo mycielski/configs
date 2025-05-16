1295983e1ae25391a6a611fc9582d998494e236c
Tomasz Mycielski <76878490+mycielski@users.noreply.github.com>
2025-05-16 17:14:19 +0200

    feat: path

---
+++ path/refresh_path.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+
+script_dir="$(cd "$(dirname "$0")" && pwd)"
+
+############
+# binaries #
+############
+echo "$0 >> Dumping list of binaries in path"
+echo "$PATH" | tr ':' '\0' | xargs -0 -I{} find "{}" 2>/dev/null | sort | uniq >"$script_dir/enumerated" &
+
+########
+# path #
+########
+echo "$0 >> Dumping \$PATH"
+echo "$PATH" >"$script_dir/path"
+
+wait
+
+##################
+# commit to repo #
+##################
+(
+	cd "$script_dir" &&
+		if [[ -n $(git status --short .) ]]; then
+			echo "$0 >> Commiting configs to git" &&
+				git add enumerated &&
+				git add path
+			git commit -S -m "chore: refresh \$PATH data" &&
+				echo "$0 >> Changes commited"
+		else
+			echo "$0 >> Configs already up-to-date in remote"
+		fi
+)
